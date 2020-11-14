acc99946f84508e2b7104f6f906ce6c5ead58ddd
raaynaldo <raaynaldo@hotmail.com>
2020-11-14 18:58:12 -0500

    commit tree c10b00118ac829c86623e1d9f06ef9e30b631a02

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name){
+    drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver() {
+    drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+    drivers.shift();
+}
+
+function appendDriver(name) {
+    const newArr = drivers.slice();
+    newArr.push(name);
+    return newArr;
+}
+
+function prependDriver(name) {
+    const newArr = drivers.slice();
+    newArr.unshift(name);
+    return newArr;
+}
+
+function removeLastDriver(name) {
+    const newArr = drivers.slice();
+    newArr.pop();
+    return newArr;
+}
+
+function removeFirstDriver(name) {
+    const newArr = drivers.slice()
+    newArr.shift()
+    return newArr;
+}
